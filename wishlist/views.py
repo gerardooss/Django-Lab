c9477e1264e6d91529f98172f86625fb890659fa
marcelinus.gerardo <marcelinus.gerardo!ui.ac.id>
2022-10-06 14:43:15 +0700

    ga selesai

--- wishlist/views.py
+++ wishlist/views.py
@@ -29,6 +29,17 @@ def show_wishlist(request):
 
     return render(request, "wishlist.html", context)
 
+@login_required(login_url='/wishlist/login/')
+def show_ajax_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Marcelinus Gerardo',
+        'last_login': request.COOKIES['last_login'],
+    }
+
+    return render(request, "wishlist_ajax.html", context)
+
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
